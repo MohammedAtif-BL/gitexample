6dfb9ef2224616c1623aa73ede391485d65ba25e
MohammedAtif-BL <mohammed.atif@bridgelabz.com>
2024-06-25 11:06:36 +0530

    Added:Employee Present or Absent

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,5 +1,12 @@
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome TO Employee Wage");
+        int num = 1;
+        if(num == 1 ) {
+            System.out.println("Employee Present");
+        }
+        else{
+            System.out.println("Emplouyee Absent");
+        }
     }
 }

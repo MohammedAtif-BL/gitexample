86b4704009c24bbb36a752cf96d393125015e916
MohammedAtif-BL <mohammed.atif@bridgelabz.com>
2024-06-25 11:11:45 +0530

    Added: Employee Wage

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -2,8 +2,11 @@ public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome TO Employee Wage");
         int num = 1;
+        double wage = 20;
+        final int FULL_TIME = 8;
         if(num == 1 ) {
-            System.out.println("Employee Present");
+            double totalwage = wage * FULL_TIME;
+            System.out.println(totalwage);
         }
         else{
             System.out.println("Emplouyee Absent");
